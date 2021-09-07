f1eecf35c499455bc3ebcf00902edcd85a541cb3
Doga <61098483+dogaegeozden@users.noreply.github.com>
2021-09-06 21:27:22 -0700

    Delete greetings directory

--- greetings/greetings.kt
+++
@@ -1,6 +0,0 @@
-fun main(){
-  var name: String?; // var/val variableName: type to create variables var(mutable) val not
-  print("Please enter your name: ") // print()/println() to print out value. print() will print in same line, println() will pass to next line after it finished printing the statement/value.
-  name = readLine() // readLine to get input from user.
-  println("Greeints ${name}.\n") // ${variableName} to use functions or variables in strings. \n for new line.
-}
